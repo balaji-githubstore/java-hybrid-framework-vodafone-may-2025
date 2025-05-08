b227321d8c0123bbd4697669dfe3b624140902d4
balaji-githubstore <balaji0017@gmail.com>
2025-05-08 11:49:57 +0530

    Added LoginTest

--- src/test/java/com/vodafone/test/LoginTest.java
+++ src/test/java/com/vodafone/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.vodafone.test;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.vodafone.base.AutomationWrapper;
@@ -11,9 +12,23 @@ public class LoginTest extends AutomationWrapper {
 	public void validLoginTest()
 	{
 		driver.findElement(By.name("username")).sendKeys("Admin");
-		//password as admin123
-		//click on login
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 		//Assert the text - Quick Launch
+		String actualText= driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
+		Assert.assertEquals(actualText, "Quick Launch");
 	}
 
+	@Test
+	public void invalidLoginTest()
+	{
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		
+		//Assert the error - Invalid credentials
+		String actualError= driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, "Invalid credentials");
+	}
+	
 }
