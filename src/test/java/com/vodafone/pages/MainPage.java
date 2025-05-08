3867b3337081d49ded741cfd5c63dfe8b572a3d7
balaji-githubstore <balaji0017@gmail.com>
2025-05-08 13:05:13 +0530

    Added Page Object Model

---
+++ src/test/java/com/vodafone/pages/MainPage.java
@@ -0,0 +1,13 @@
+package com.vodafone.pages;
+
+import org.openqa.selenium.WebDriver;
+/**
+ * All menu will be handled here
+ */
+public class MainPage {
+	private WebDriver driver;
+	
+	public MainPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
--- src/test/java/com/vodafone/test/LoginTest.java
+++ src/test/java/com/vodafone/test/LoginTest.java
@@ -1,34 +1,34 @@
 package com.vodafone.test;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.vodafone.base.AutomationWrapper;
+import com.vodafone.pages.DashboardPage;
+import com.vodafone.pages.LoginPage;
 
 public class LoginTest extends AutomationWrapper {
-	
+
 	@Test
-	public void validLoginTest()
-	{
-		driver.findElement(By.name("username")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-		//Assert the text - Quick Launch
-		String actualText= driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
-		Assert.assertEquals(actualText, "Quick Launch");
+	public void validLoginTest() {
+		LoginPage login = new LoginPage(driver);
+		login.enterUsername("Admin");
+		login.enterPassword("admin123");
+		login.clickOnLogin();
+
+		DashboardPage dashboard = new DashboardPage(driver);
+		Assert.assertEquals(dashboard.getQuickLaunchText(), "Quick Launch");
 	}
 
 	@Test
-	public void invalidLoginTest()
-	{
-		driver.findElement(By.name("username")).sendKeys("john");
-		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-		
-		//Assert the error - Invalid credentials
-		String actualError= driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+	public void invalidLoginTest() {
+		LoginPage login = new LoginPage(driver);
+		login.enterUsername("john");
+		login.enterPassword("admin123");
+		login.clickOnLogin();
+
+		String actualError = login.getInvalidErrorMessage();
 		Assert.assertEquals(actualError, "Invalid credentials");
 	}
-	
+
 }
